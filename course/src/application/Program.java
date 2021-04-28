43dc56f3fbd2578eae934934593990ece447593d
Trauvin <mateusgalote@gmail.com>
2021-04-28 17:24:58 -0300

    working with files

---
+++ course/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("/tmp/a.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close(); // fecha o scanner independente do que aconteça
+			}
+		}
+	}
+
+}
